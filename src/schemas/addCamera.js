d109bc2beea6a69e54630c16db156697f7bde160
Janek Rahrt <janek.rahrt@me.com>
2019-07-25 20:54:40 +0200

    add test and ipv6 support

--- src/schemas/addCamera.js
+++ src/schemas/addCamera.js
@@ -18,17 +18,44 @@ export const portErrors = {
   max: 'Ein Port kann maximal 65535 lauten',
 }
 
-export const matchesHostnamesOrIp = /(^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$)|(^(?!:\/\/)([a-zA-Z0-9-_]+\.)*[a-zA-Z0-9][a-zA-Z0-9-_]+\.[a-zA-Z]{2,11}?$)/
+export const matchesIpv4s = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
+export const matchesPrivateIpv4s = /^(^127\.)|(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.).*$/
+export const matchesIpv6s = /^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$/
+export const matchesPrivateIpv6s = /^[0:]+1$/
+export const matchesHostnames = /^(?!:\/\/)([a-zA-Z0-9-_]+\.)*[a-zA-Z0-9][a-zA-Z0-9-_]+\.[a-zA-Z]{2,11}?$/
 
 export const hostValidation = yup
   .string()
   .required(addressErrors.required)
   .trim()
-  .matches(matchesHostnamesOrIp, addressErrors.url)
-  .matches(
-    /^((?!((^127\.)|(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.))).)*$/,
-    addressErrors.local,
-  )
+  .test('is-valid-host', addressErrors.url, host => {
+    const isIpv4 = yup
+      .string()
+      .matches(matchesIpv4s)
+      .isValidSync(host)
+    const isIpv6 = yup
+      .string()
+      .matches(matchesIpv6s)
+      .isValidSync(host)
+    const isHostname = yup
+      .string()
+      .matches(matchesHostnames)
+      .isValidSync(host)
+
+    return isIpv4 || isIpv6 || isHostname
+  })
+  .test('is-public-host', addressErrors.local, host => {
+    const isLocalIpv4 = yup
+      .string()
+      .matches(matchesPrivateIpv4s)
+      .isValidSync(host)
+    const isLocalIpv6 = yup
+      .string()
+      .matches(matchesPrivateIpv6s)
+      .isValidSync(host)
+
+    return !(isLocalIpv4 || isLocalIpv6)
+  })
 
 export const portValidation = yup
   .number()
@@ -48,5 +75,5 @@ export default yup
     usr: yup.string().ensure(),
     pwd: yup.string().ensure(),
     http: portValidation.required(addressErrors.required),
-    rtsp: portValidation.max(65535, portErrors.max),
+    rtsp: portValidation,
   })
